a740a1d55a00c4169e03e51f94bedcecd9189c17
Wieslaw Kubala <wk@tednet.pl>
2022-11-27 20:05:30 +0100

    Drawing chart from database
    Drawing weather forecast from data saved to database: avg, min and max temperature

--- WpfApp1/WpfApp1/MainViewModel.cs
+++ WpfApp1/WpfApp1/MainViewModel.cs
@@ -9,10 +9,15 @@
 
 namespace WpfApp1
 {
+    using Newtonsoft.Json;
     using OxyPlot;
     using OxyPlot.Axes;
     using OxyPlot.Series;
     using System;
+    using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+    using System.Data.SQLite;
+    using System.Text.Json;
+    using System.Xml.Linq;
 
     /// <summary>
     /// Represents the view-model for the main window.
@@ -22,6 +27,13 @@ namespace WpfApp1
         /// <summary>
         /// Initializes a new instance of the <see cref="MainViewModel" /> class.
         /// </summary>
+
+        public static DateTime UnixTimestampToDateTime(double unixTime)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks = (long)(unixTime * TimeSpan.TicksPerSecond);
+            return new DateTime(unixStart.Ticks + unixTimeStampInTicks, System.DateTimeKind.Utc);
+        }
         public MainViewModel()
         {
             // Create the plot model
@@ -33,7 +45,6 @@ namespace WpfApp1
 //                StringFormat = "dd/MM/yyyy HH:mm",
                 StringFormat = "dd/MM HH:mm",
 
-                //                Title = "Year",
                 MinorIntervalType = DateTimeIntervalType.Days,
                 IntervalType = DateTimeIntervalType.Days,
                 MajorGridlineStyle = LineStyle.Solid,
@@ -41,27 +52,73 @@ namespace WpfApp1
             };
             tmp.Axes.Add(xAxis);
 
-            DateTime now = DateTime.Now;
+            var series1 = new LineSeries { Title = "Min temperature", MarkerType = MarkerType.Circle };
+            var series2 = new LineSeries { Title = "Max temperature", MarkerType = MarkerType.Square };
+            var series3 = new LineSeries { Title = "Avg temperature", MarkerType = MarkerType.Triangle };
 
-            // Create two line series (markers are hidden by default)
-            var series1 = new LineSeries { Title = "Series 1", MarkerType = MarkerType.Circle };
-            series1.Points.Add(new DataPoint(now.ToOADate(), 0));
-            series1.Points.Add(new DataPoint(now.AddDays(1).ToOADate(), -18));
-            series1.Points.Add(new DataPoint(now.AddDays(2).ToOADate(), -12));
-            series1.Points.Add(new DataPoint(now.AddDays(3).ToOADate(), -8));
-            series1.Points.Add(new DataPoint(now.AddDays(4).ToOADate(), -15));
+            string K2_lat = "35.88";
+            string K2_lon = "76.51";
+            string units = "metric";
 
-            var series2 = new LineSeries { Title = "Series 2", MarkerType = MarkerType.Square };
-            series2.Points.Add(new DataPoint(now.ToOADate(), -4));
-            series2.Points.Add(new DataPoint(now.AddDays(1).ToOADate(), -12));
-            series2.Points.Add(new DataPoint(now.AddDays(2).ToOADate(), -16));
-            series2.Points.Add(new DataPoint(now.AddDays(3).ToOADate(), -25));
-            series2.Points.Add(new DataPoint(now.AddDays(4).ToOADate(), -5));
+            int positionId = 0;
+            int unitsId = 0;
 
+            SQLiteDB db = new SQLiteDB();
+            using(var connection = db.createDbConnection())
+            {
+                db.command.Parameters.Clear();
+                db.command.CommandText = "select id from LatLonPositions where lat = @lat and lon = @lon";
+                db.command.Parameters.AddWithValue("@lat", K2_lat);
+                db.command.Parameters.AddWithValue("@lon", K2_lon);
+
+
+                SQLiteDataReader reader = db.command.ExecuteReader();
+                if (reader.Read())
+                    positionId = reader.GetInt32(0);
+                reader.Close();
+
+                db.command.Parameters.Clear();
+                db.command.CommandText = "select id from Units where key = @key";
+                db.command.Parameters.AddWithValue("@key", units);
+                reader = db.command.ExecuteReader();
+                if (reader.Read())
+                    unitsId = reader.GetInt32(0);
+                reader.Close();
+
+                string json = "";
+
+                db.command.Parameters.Clear();
+                db.command.CommandText = "select json from ForecastJsons where unitId = @unitId and positionId = @positionId";
+                db.command.Parameters.AddWithValue("@unitId", unitsId);
+                db.command.Parameters.AddWithValue("@positionId", positionId);
+                reader = db.command.ExecuteReader();
+                if (reader.Read())
+                    json = reader.GetString(0);
+                reader.Close();
+
+                dynamic stuff = JsonConvert.DeserializeObject(json);
+
+                for(int i = 0; i < stuff.list.Count; i++)
+                {
+                    long dt = stuff.list[i].dt;
+                    double temp = stuff.list[i].main.temp;
+                    double temp_min = stuff.list[i].main.temp_min;
+                    double temp_max = stuff.list[i].main.temp_max;
+                    string dt_txt = stuff.list[i].dt_txt;
+                    DateTime dt2 = UnixTimestampToDateTime(dt);
+
+                    series1.Points.Add(new DataPoint(dt2.ToOADate(), temp_min));
+                    series2.Points.Add(new DataPoint(dt2.ToOADate(), temp_max));
+                    series3.Points.Add(new DataPoint(dt2.ToOADate(), temp));
+                }
+
+
+            }
 
             // Add the series to the plot model
             tmp.Series.Add(series1);
             tmp.Series.Add(series2);
+            tmp.Series.Add(series3);
 
             // Axes are created automatically if they are not defined
 
